254b04999b8c0d194eb2192553b3eaa594384940
stray-code <abc1007020@gmail.com>
2024-03-21 21:41:49 +0900

    ダミーデータを作成

---
+++ src/data/dummyTodoList.ts
@@ -0,0 +1,19 @@
+import { Todo } from '../types/todo';
+
+export const dummyTodoList: Todo[] = [
+  {
+    id: 1,
+    title: 'Reactを勉強する',
+    completed: false,
+  },
+  {
+    id: 2,
+    title: '夕食を作る',
+    completed: false,
+  },
+  {
+    id: 3,
+    title: '部屋を掃除する',
+    completed: true,
+  },
+];
