b45c70c01aa9c00db3e114d7e172914eb613cf00
stray-code <abc1007020@gmail.com>
2024-03-21 21:53:45 +0900

    Todoにチェックボックスを表示

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -8,7 +8,21 @@ export const TodoList = ({ todoList }: Props) => {
   return (
     <div className="space-y-3">
       {todoList.map((todo) => (
-        <p key={todo.id}>{todo.title}</p>
+        <div
+          key={todo.id}
+          className="flex items-center gap-3 rounded bg-white p-2"
+        >
+          <label className="flex grow items-center gap-3 hover:cursor-pointer">
+            <div className="flex items-center">
+              <input
+                type="checkbox"
+                className="size-5"
+                checked={todo.completed}
+              />
+            </div>
+            {todo.title}
+          </label>
+        </div>
       ))}
     </div>
   );
