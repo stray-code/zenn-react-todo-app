2c2f0de24b52a7f57407a41b64204103c0773239
stray-code <abc1007020@gmail.com>
2024-03-21 21:43:13 +0900

    ダミーデータを表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { dummyTodoList } from './data/dummyTodoList';
+
 function App() {
   return (
-    <main className="mx-auto mt-10 max-w-xl">
+    <main className="mx-auto mt-10 max-w-xl space-y-10">
       <h1 className="text-center text-4xl">Todoアプリ</h1>
+      <div className="rounded bg-slate-200 p-5">
+        <div className="space-y-3">
+          {dummyTodoList.map((todo) => (
+            <p key={todo.id}>{todo.title}</p>
+          ))}
+        </div>
+      </div>
     </main>
   );
 }
